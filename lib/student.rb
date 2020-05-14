5af5fa5985c0245af41ba648aa4e125ca763d5f8
Megan_Comeau <mlcomeau99@gmail.com>
2020-05-14 14:26:06 -0700

    Tests passing

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,29 @@ class Student
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade=10
-    LIMIT=?
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade=10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end
+
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade=?
     SQL
 
     DB[:conn].execute(sql, num).map do |row|
